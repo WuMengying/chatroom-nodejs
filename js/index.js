3f0635d3c37689ac15eff7b4a42a729cb9c97580
CyanChann <664201614@qq.com>
2017-06-25 13:01:07 +0800

    2017.06.25 update

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
+// 获取url里面的内容
 var url = decodeURI(location.href).split('?')[1].split('&');
+
+// 获取聊天内容框
 var chatContent = document.getElementsByClassName('chat-content')[0];
+
+// 获取聊天输入框
 var editBox = document.getElementsByClassName('edit-box')[0];
+
+// 获取聊天输入框发送按钮
 var editButton = document.getElementsByClassName('edit-button')[0];
+
+// 获取用户名栏
 var userName = document.getElementsByClassName('user-name')[0];
+
+// 获取在线人数栏
+var onlineCount = document.getElementsByClassName('online-count')[0];
+
 // 把登录页面的名称放在右侧
 userName.innerHTML = url[1].split('=')[1];
 var userImg = document.getElementsByClassName('user-img')[0];
+
 // 把登录页面的头像放在右侧
 userImg.src = 'img/' + url[0].split('=')[1];
 var logOut = document.getElementsByClassName('log-out')[0];
-// 0.8s获取有无新的聊天内容
-var get = setInterval(getTime, 800);
+
 // 发送按钮绑定点击事件
 editButton.addEventListener('click', sendMessage);
+
 // 登出按钮绑定点击事件
 logOut.addEventListener('click', closePage);
+
 // 绑定Enter键和发送事件
 document.onkeydown = function (event) {
     var e = event || window.event;
@@ -24,6 +39,7 @@ document.onkeydown = function (event) {
         }
     }
 };
+
 // 关闭页面
 function closePage() {
     var userAgent = navigator.userAgent;
@@ -35,184 +51,105 @@ function closePage() {
         window.close();
     }
 }
+// socket部分
+var socket = io();
+
+// 当接收到消息并且不是本机时生成聊天气泡
+socket.on('message', function (information) {
+    if (information.name !== userName.textContent) {
+        createOtherMessage(information);
+    }
+});
+
+// 当接收到有人连接进来
+socket.on('connected', function (onlinecount) {
+    console.log(onlinecount);
+    onlineCount.innerHTML = 'Online:' + onlinecount;
+});
+
+// 当接收到有人断开后
+socket.on('disconnected', function (onlinecount) {
+    console.log(onlinecount);
+    onlineCount.innerHTML = 'Online:' + onlinecount;
+});
 
 // 发送本机的消息
 function sendMessage() {
-    $.ajax({
-        url: "http://119.29.3.75:8080/chatroom/1",
-        type: "get",
-        dataType: "jsonp",
-        jsonp: "jsoncallback",
-        jsonpCallback: "jsonpCallback_success",
-        data: {
-            'username': encodeURI(url[1].split('=')[1]),
-            'content': encodeURI(editBox.value),
-            'picture': encodeURI('img/' + url[0].split('=')[1])
-        },
-        success: function (data) {
-            if (data.time != null && data.time != undefined && encodeURI(editBox.value) != null) {
-                createMyMessage(data);
-            }
-        },
-        error: function () {
-            console.log("error");
-        }
-    });
-}
+    if (editBox.value != '') {
+        var myInformation = {
+            name: userName.textContent,
+            chatContent: editBox.value,
+            img: userImg.src
+        };
+        socket.emit('message', myInformation);
+        createMyMessage();
+        editBox.value = '';
+    }
 
-// 生成本机发送的消息气泡
-//<div class="my-message-box">
-//     <ul class="message-arrow">
-//         <li>
-//              <span class="my-message">message</span>
-//             <div class="my-arrow"></div>
-//              <span class="time">time</span>
-//          </li>
-//         <li>
-//              <img class="message-img" src="img/x.png">
-//              <span class="name">name</span>
-//          </li>
-//     </ul>
-// </div>
-
-function createMyMessage(data) {
-    // 最外层
-    var messageBox = document.createElement('div');
-    messageBox.setAttribute('class', 'my-message-box');
-    // 内层ul
-    var ul = document.createElement('ul');
-    ul.setAttribute('class', 'message-arrow');
-    // 消息内容
-    var li1 = document.createElement('li');
-    var myMessage = document.createElement('span');
-    myMessage.setAttribute('class', 'my-message');
-    myMessage.innerHTML = editBox.value;
-    li1.appendChild(myMessage);
-    // 气泡箭头
-    var arrow = document.createElement('div');
-    arrow.setAttribute('class', 'my-arrow');
-    li1.appendChild(arrow);
-    // 消息时间
-    var time = document.createElement('span');
-    time.setAttribute('class', 'time');
-    time.innerHTML = data.time;
-    li1.appendChild(time);
-    // 头像
-    var li2 = document.createElement('li');
-    var userimg = document.createElement('img');
-    userimg.setAttribute('class', 'message-img');
-    userimg.src = userImg.src;
-    li2.appendChild(userimg);
-    // 用户名
-    var name = document.createElement('span');
-    name.setAttribute('class', 'name');
-    name.innerHTML = userName.textContent;
-    li2.appendChild(name);
-    // 打包所有内容
-    ul.appendChild(li1);
-    ul.appendChild(li2);
-    messageBox.appendChild(ul);
-    // 插入到聊天框
-    chatContent.appendChild(messageBox);
-    // 清除发送后的输入框内容
-    editBox.value = '';
-    // 把聊天内容框拉到最下面
-    chatContent.scrollTop = chatContent.scrollHeight;
-}
-
-// 生成其他用户的消息气泡
-//<div class="other-message-box">
-//    <ul class="message-arrow">
-//        <li>
-//          <img class="message-img" src="img/x.png">
-//          <span class="name">name</span>
-//         </li>
-//        <li>
-//            <div class="other-arrow"></div>
-//             <span class="other-message">message</span>
-//              <span class="time">time</span>
-//          </li>
-//    </ul>
-//</div>
-
-function createOtherMessage(data) {
-    // 最外层
-    var messageBox = document.createElement('div');
-    messageBox.setAttribute('class', 'other-message-box');
-    // 内层
-    var ul = document.createElement('ul');
-    ul.setAttribute('class', 'message-arrow');
-    var li1 = document.createElement('li');
-    // 头像
-    var userimg = document.createElement('img');
-    userimg.setAttribute('class', 'message-img');
-    userimg.src = data.picture;
-    li1.appendChild(userimg);
-    // 用户名
-    var name = document.createElement('span');
-    name.setAttribute('class', 'name');
-    name.innerHTML = data.name;
-    li1.appendChild(name);
-    var li2 = document.createElement('li');
-    // 箭头
-    var arrow = document.createElement('div');
-    arrow.setAttribute('class', 'other-arrow');
-    li2.appendChild(arrow);
-    // 气泡消息
-    var otherMessage = document.createElement('span');
-    otherMessage.setAttribute('class', 'other-message');
-    otherMessage.innerHTML = data.content;
-    li2.appendChild(otherMessage);
-    // 时间
-    var time = document.createElement('span');
-    time.setAttribute('class', 'time');
-    var now = new Date();
-    time.innerHTML = data.time;
-    li2.appendChild(time);
-    // 打包所有内容
-    ul.appendChild(li1);
-    ul.appendChild(li2);
-    messageBox.appendChild(ul);
-    // 插入到聊天框
-    chatContent.appendChild(messageBox);
-    // 把聊天内容框拉到最下面
-    chatContent.scrollTop = chatContent.scrollHeight;
-}
-// 利用服务器当前事件请求聊天内容
-function getMessage(time) {
-    $.ajax({
-        url: "http://119.29.3.75:8080/chatroom/2",
-        type: "get",
-        dataType: "jsonp",
-        jsonp: "jsoncallback",
-        jsonpCallback: "jsonpCallback_success",
-        data: {
-            'time': time
-        },
-        success: function (data) {
-            console.log(data);
-            if (data.content !== 'null' & userName.textContent !== data.name) {
-                createOtherMessage(data);
-            }
-        },
-        error: function () {
-            console.log("error");
-        }
-    });
 };
-// 获取服务器当前事件
-function getTime() {
-    $.ajax({
-        url: "http://119.29.3.75:8080/chatroom/3",
-        type: "get",
-        dataType: "jsonp",
-        jsonp: "jsoncallback",
-        jsonpCallback: "jsonpCallback_success",
-        success: function (data) {
-            getMessage(data.time)
-        },
-        error: function () {
-            console.log("error");
-        }
-    });
+
+// 生成本机的聊天气泡
+function createMyMessage() {
+    var myMessageBox = document.createElement('div');
+    myMessageBox.className = 'my-message-box';
+
+    var messageContent = document.createElement('div');
+    messageContent.className = 'message-content';
+    var text = document.createElement('span');
+    text.innerHTML = editBox.value;
+    messageContent.appendChild(text);
+    myMessageBox.appendChild(messageContent);
+
+    var arrow = document.createElement('div')
+    arrow.className = 'message-arrow';
+    myMessageBox.appendChild(arrow);
+
+    var userInformation = document.createElement('div');
+    userInformation.className = 'user-information';
+    var userChatImg = document.createElement('img');
+    userChatImg.className = 'user-chat-img';
+    userChatImg.src = userImg.src;
+    var userChatName = document.createElement('div');
+    userChatName.className = 'user-chat-name';
+    userChatName.innerHTML = userName.textContent;
+    userInformation.appendChild(userChatImg);
+    userInformation.appendChild(userChatName);
+    myMessageBox.appendChild(userInformation);
+
+    chatContent.appendChild(myMessageBox);
+
+    chatContent.scrollTop = chatContent.scrollHeight;
+}
+
+// 生成其他用户的聊天气泡
+function createOtherMessage(information) {
+    var otherMessageBox = document.createElement('div');
+    otherMessageBox.className = 'other-message-box';
+
+    var otherUserInformation = document.createElement('div');
+    otherUserInformation.className = 'other-user-information';
+    var userChatImg = document.createElement('img');
+    userChatImg.className = 'user-chat-img';
+    userChatImg.src = information.img;
+    var userChatName = document.createElement('span');
+    userChatName.className = 'user-chat-name';
+    userChatName.innerHTML = information.name;
+    otherUserInformation.appendChild(userChatImg);
+    otherUserInformation.appendChild(userChatName);
+    otherMessageBox.appendChild(otherUserInformation);
+
+    var otherMessageArrow = document.createElement('div');
+    otherMessageArrow.className = 'other-message-arrow';
+    otherMessageBox.appendChild(otherMessageArrow);
+
+    var otherMessageContent = document.createElement('div');
+    otherMessageContent.className = 'other-message-content';
+    var text = document.createElement('span');
+    text.innerHTML = information.chatContent;
+    otherMessageContent.appendChild(text);
+    otherMessageBox.appendChild(otherMessageContent);
+
+    chatContent.appendChild(otherMessageBox);
+
+    chatContent.scrollTop = chatContent.scrollHeight;
 }
--- js/login.js
+++ js/login.js
@@ -1,21 +1,35 @@
-var imgArray = ['1', '2', '3', '4', '5']; //用于存储图片顺序
+//用于存储图片顺序
+var imgArray = ['1', '2', '3', '4', '5']; 
+
+//获取箭头
 var leftArrow = document.getElementsByClassName('left-arrow')[0];
 var rightArrow = document.getElementsByClassName('right-arrow')[0];
+
+//获取用户名
 var userName = document.getElementsByClassName('user-name')[0];
+
+//获取登录按钮
 var loginButton = document.getElementsByClassName('login-button')[0];
+
+// 获取错误信息栏
 var errorMessage = document.getElementsByClassName('error-message')[0];
+
 // 添加左箭头监听事件
 leftArrow.addEventListener('click', function () {
-    imgArray.unshift(imgArray[imgArray.length - 1]);//把最后的元素放在第一位
+    imgArray.unshift(imgArray[imgArray.length - 1]); //把最后的元素放在第一位
     imgArray.pop();
     carouselImg();
+
 });
+
 // 添加右箭头监听事件
 rightArrow.addEventListener('click', function () {
-    imgArray.push(imgArray[0]);//把第一个元素放在最后
+    imgArray.push(imgArray[0]); //把第一个元素放在最后
     imgArray.shift();
     carouselImg();
+
 });
+
 // 切换图片
 function carouselImg() {
     for (var count = 0; count < imgArray.length; count++) {
@@ -23,6 +37,7 @@ function carouselImg() {
         document.getElementsByTagName('img')[count].alt = imgArray[count] + '.png';
     };
 };
+
 // 添加登录按钮监听事件
 loginButton.addEventListener('click', function () {
     if (userName.value === '') {
@@ -37,6 +52,7 @@ loginButton.addEventListener('click', function () {
                 '&username=' + userName.value);
     }
 });
+
 // Enter按键绑定登录事件
 document.onkeydown = function (event) {
     var e = event || window.event;
